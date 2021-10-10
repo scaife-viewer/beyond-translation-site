a0ebcf7b8d98750be169113ad9d8266147dda329
Jacob Wegner <jacobwegner@gmail.com>
2021-10-10 08:37:45 -0500

    Write metadata stubs

--- backend/scaife_stack_atlas/extractors/extract_gorman_text.py
+++ backend/scaife_stack_atlas/extractors/extract_gorman_text.py
@@ -55,17 +55,85 @@ def get_paths():
     return syntax_trees_dir.glob("gorman_*")
 
 
-def extract_text(input_path):
-    version_urn, refs_and_lines = extract_version_refs_and_lines(input_path)
+def ensure_tg_metadata(version_urn, tg_path):
+    metadata_path = Path(tg_path, "metadata.json")
+    if not metadata_path.exists():
+        data = {
+            "urn": version_urn.up_to(URN.TEXTGROUP),
+            "node_kind": "textgroup",
+            "name": [{"lang": "eng", "value": version_urn.parsed["textgroup"],}],
+        }
+        with metadata_path.open("w") as f:
+            json.dump(data, f, indent=2)
+
+
+def ensure_work_metadata(version_urn, w_path):
+    metadata_path = Path(w_path, "metadata.json")
+    if not metadata_path.exists():
+        data = {
+            "urn": version_urn.up_to(URN.WORK),
+            "group_urn": version_urn.up_to(URN.TEXTGROUP),
+            "node_kind": "work",
+            "lang": "grc",  # TODO: Extract this?
+            "title": [{"lang": "eng", "value": version_urn.parsed["work"]}],
+            "versions": [
+                {
+                    "urn": str(version_urn),
+                    "node_kind": "version",
+                    "version_kind": "edition",
+                    "lang": "grc",  # TODO: Extract this?
+                    "first_passage_urn": f"{version_urn}1",
+                    "citation_scheme": ["sentence"],
+                    "label": [
+                        {
+                            "lang": "eng",
+                            "value": f'{version_urn.parsed["version"]} (sentences)',
+                        }
+                    ],
+                    "description": [
+                        {"lang": "eng", "value": "Extracted from vgorman1 trees",}
+                    ],
+                }
+            ],
+        }
+        with metadata_path.open("w") as f:
+            json.dump(data, f, indent=2)
+
+
+def stub_metadata(version_urn):
+    tgp = version_urn.parsed["textgroup"]
+    wp = version_urn.parsed["work"]
+
+    tg_path = Path(f"data/library/{tgp}")
+    ensure_tg_metadata(version_urn, tg_path)
+
+    w_path = Path(tg_path, wp)
+    version_part = str(version_urn)[:-1]
+    exemplar_part = "vgorman1-trees"
+    versionish_urn = URN(f"{version_part}-{exemplar_part}:")
+    ensure_work_metadata(versionish_urn, w_path)
+
+
+def get_output_path(version_urn):
     tgp = version_urn.parsed["textgroup"]
     wp = version_urn.parsed["work"]
     vp = version_urn.parsed["version"]
-    output_path = Path(f"data/library/{tgp}/{wp}/{tgp}.{wp}.{vp}-vgorman1-trees.txt")
+    workpart_path = Path(f"data/library/{tgp}/{wp}")
+    version_part = f"{vp}-vgorman1-trees"
+    return Path(workpart_path, f"{tgp}.{wp}.{version_part}.txt")
+
+
+def extract_text(input_path):
+    version_urn, refs_and_lines = extract_version_refs_and_lines(input_path)
+    output_path = get_output_path(version_urn)
+
+    # TODO: Replace with actual metadata
+    stub_metadata(version_urn)
+
     # TODO: Handle where output path already exists; likely we need to
     # combine texts when we're doing the extraction
     output_path.parent.mkdir(parents=True, exist_ok=True)
     write_text(output_path, refs_and_lines)
-    # TODO: Write out metadata too
 
 
 def main():
