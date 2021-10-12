466c858aaca96c8e889df46101f6f974d2216aac
Jacob Wegner <jwegner@eldarion.com>
2021-10-12 16:52:47 -0500

    Synchronize sentence exemplar references

--- backend/scaife_stack_atlas/extractors/update_gorman_references.py
+++ backend/scaife_stack_atlas/extractors/update_gorman_references.py
@@ -1,8 +1,10 @@
 import json
 import os
+from collections import defaultdict
 from pathlib import Path
 
 import django
+
 
 # TODO: refactor this as an actual Django management command
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scaife_stack_atlas.settings")
@@ -13,10 +15,10 @@ from scaife_viewer.atlas.urn import URN  # noqa
 
 def get_paths():
     syntax_trees_dir = Path("data/annotations/syntax-trees")
-    return syntax_trees_dir.glob("gorman_*")
+    return sorted(syntax_trees_dir.glob("gorman_*"))
 
 
-def update_references(input_path):
+def update_references(workpart_counter, input_path):
     sentences = json.load(input_path.open())
     version_urn = None
     for sentence in sentences:
@@ -31,17 +33,20 @@ def update_references(input_path):
     exemplar_part = "vgorman1-trees"
     version = f"{version_part}-{exemplar_part}:"
 
+    work_urn = str(version_urn.up_to(URN.WORK))
+    counter = workpart_counter[work_urn]
     for sentence in sentences:
-        references = sentence["references"]
-        if references:
-            passage = f'{version}{sentence["treebank_id"]}'
-            sentence["references"] = [passage]
+        passage = f"{version}{counter + 1}"
+        sentence["references"] = [passage]
+        counter += 1
+    workpart_counter[work_urn] = counter
     json.dump(sentences, input_path.open("w"), indent=2, ensure_ascii=False)
 
 
 def main():
+    workpart_counter = defaultdict(int)
     for path in get_paths():
-        update_references(path)
+        update_references(workpart_counter, path)
 
 
 if __name__ == "__main__":
