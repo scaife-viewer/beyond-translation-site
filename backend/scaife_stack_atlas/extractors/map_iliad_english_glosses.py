4021f0a033eaada159c3c1410f5410bfae5359ee
Jacob Wegner <jwegner@eldarion.com>
2022-01-26 14:44:31 -0600

    Update from helmadik/shortdefs

--- backend/scaife_stack_atlas/extractors/map_iliad_english_glosses.py
+++ backend/scaife_stack_atlas/extractors/map_iliad_english_glosses.py
@@ -5,23 +5,24 @@ from scaife_viewer.atlas.language_utils import normalize_string
 
 
 def main():
-    eng_glosses_path = Path("data/raw/homer-dik/vocab-perseus-logeion.csv")
+    eng_glosses_path = Path("data/raw/homer-dik/shortdefsGreekEnglishLogeion.tsv")
     iliad_annotations_path = Path(
         "data/annotations/token-annotations/iliad-crane-shamsian/tlg0012.tlg001.perseus-grc2.csv"
     )
 
     gloss_lu = {}
-    gloss_reader = iter(csv.reader(eng_glosses_path.open(encoding="utf-8-sig")))
+    gloss_reader = iter(csv.DictReader(eng_glosses_path.open(encoding="utf-8-sig")))
     for row in gloss_reader:
-        # Store the normalized gloss form
-        gloss_lu[normalize_string(row[1])] = row[2]
+        lemma = normalize_string(row["lemma"])
+        # Store the normalized lemma form
+        gloss_lu[lemma] = row["def"]
 
     updated_rows = []
     annotation_reader = csv.DictReader(
         iliad_annotations_path.open(encoding="utf-8-sig")
     )
     for row in annotation_reader:
-        # lookup using the normalized gloss form
+        # lookup using the normalized lemma form
         row["gloss (eng)"] = gloss_lu.get(normalize_string(row["lemma"]), "")
         updated_rows.append(row)
 
