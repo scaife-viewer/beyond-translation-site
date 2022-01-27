76711ecaf40b93313e61682f3fd314df25d5abb9
Jacob Wegner <jwegner@eldarion.com>
2022-01-27 06:21:56 -0600

    Use lemmas to resolve dictionary entries

--- backend/manage.py
+++ backend/manage.py
@@ -6,6 +6,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scaife_stack_atlas.settings")
+    # FIXME: Local dev hack
+    os.environ.setdefault("SV_ATLAS_RESOLVE_DICTIONARY_ENTRIES_VIA_LEMMAS", "1")
 
     from django.core.management import execute_from_command_line
 
