b4a6436cb4a556bae5430309dbe26a5b4afe671b
Jacob Wegner <jwegner@eldarion.com>
2022-03-02 20:47:10 -0600

    Second pass using syntax tree data
    We don't have 100% of the tokens annotated; will circle back
    for more error logging

--- backend/scaife_stack_atlas/extractors/extract_treebank_annotations.py
+++ backend/scaife_stack_atlas/extractors/extract_treebank_annotations.py
@@ -2,15 +2,16 @@
 Script used to extract Odyssey treebank annotations
 """
 from collections import Counter
-import csv
+import json
 from pathlib import Path
 
+from scaife_viewer.atlas import tokenizers
 from scaife_stack_atlas.postag_convert import deep_morphology_pos_and_parse
 
 
 def main():
     input_path = Path(
-        "/Users/jwegner/Data/development/repos/gregorycrane/Homerica/tlg0012-tbankplus.txt"
+        "data/annotations/syntax-trees/gregorycrane_gagdt_syntax_trees_tlg0012.tlg002.perseus-grc2.json"
     )
     odyssey_annotations_path = Path(
         "data/annotations/token-annotations/od-crane/tlg0012.tlg002.perseus-grc2.csv"
@@ -23,42 +24,42 @@ def main():
         "lemma",
         "part_of_speech",
         "parse",
-        "tag"
+        "tag",
     ]
     annotations = []
     refcounter = Counter()
     with input_path.open() as f:
-        reader = csv.reader(f, delimiter="\t")
-        for row in reader:
-            try:
-                _, urn, tree_id, lemma, form, postag, _, _, _ = row
-            except:
-                print(row)
-                continue
+        data = json.load(f)
+        for row in data:
+            for word in row["words"]:
+                annotation = dict()
+                try:
+                    ref = word["ref"]
+                except KeyError:
+                    print(word)
+                    continue
+                try:
+                    book, line = ref.split(".")
+                except ValueError:
+                    print(ref)
+                    continue
 
-            if row[0].startswith("#"):
-                continue
+                word_value = tokenizers.Token.get_word_value(word["value"])
+                if not word_value:
+                    continue
 
-            if not urn.startswith("urn:cts:greekLit:tlg0012.tlg002"):
-                continue
-            annotation = dict()
-            ref, subref = urn.rsplit(":", maxsplit=1)[1].split("@")
-            try:
-                book, line = ref.split(".")
-            except:
-                # TODO: fix these
-                print(urn)
-                continue
-            refcounter[ref] += 1
-            position = refcounter[ref]
-            annotation["ve_ref"] = f"{book}.{line}.t{position}"
-            # YAGNI
-            # annotation["value"] = ""
-            # annotation["word_value"] = ""
-            annotation["lemma"] = lemma
-            annotation["tag"] = postag
-            annotation["pos"], annotation["parse"] = deep_morphology_pos_and_parse(postag)
-            annotations.append(annotation)
+                refcounter[ref] += 1
+                position = refcounter[ref]
+                annotation["ve_ref"] = f"{book}.{line}.t{position}"
+                annotation["word_value"] = word_value
+                annotation["value"] = word["value"]
+                annotation["lemma"] = word["lemma"]
+                annotation["tag"] = word["tag"]
+                (
+                    annotation["part_of_speech"],
+                    annotation["parse"],
+                ) = deep_morphology_pos_and_parse(word["tag"])
+                annotations.append(annotation)
 
     with odyssey_annotations_path.open("w", encoding="utf-8-sig") as f:
         annotation_writer = csv.DictWriter(f, fieldnames=fieldnames,)
