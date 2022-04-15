01a4822536906e368850c4b8c40faf870d5f2501
Jacob Wegner <jwegner@eldarion.com>
2022-04-15 07:05:36 -0500

    First pass at "blob" entry extraction for LSJ

---
+++ backend/data/raw/lsj/lsj.xsl
@@ -0,0 +1,100 @@
+<?xml version="1.0"?>
+<xsl:stylesheet
+  version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:py="urn:python-funcs"
+  exclude-result-prefixes="py">
+
+  <xsl:output xml:space="default" method="html"/>
+
+  <xsl:template match="//entryFree">
+    <div class="entry">
+      <!-- <h2>
+        <xsl:value-of select="py:beta_to_uni(./@key)"/>
+      </h2> -->
+      <xsl:apply-templates/>
+    </div>
+    <br />
+    <br />
+  </xsl:template>
+
+  <xsl:template match="orth|foreign|quote">
+    <span>
+      <xsl:attribute name="class">
+        <xsl:value-of select="name(.)" />
+        <xsl:value-of select="' '" />
+        <xsl:value-of select="./@lang" />
+      </xsl:attribute>
+      <xsl:value-of select="py:beta_to_uni(./text())"/>
+    </span>
+  </xsl:template>
+
+  <xsl:template match="bibl">
+    <xsl:choose>
+      <xsl:when test="starts-with(@n, 'urn')">
+        <!-- TODO: Scaife versus other fallback -->
+        <a class="bibl">
+          <xsl:attribute name="target">
+            <xsl:value-of select="'_blank'" />
+          </xsl:attribute>
+          <xsl:attribute name="data-urn">
+            <xsl:value-of select="@n" />
+          </xsl:attribute>
+          <xsl:attribute name="href">
+            <xsl:value-of select="py:catalog_link(.)"/>
+          </xsl:attribute>
+          <xsl:apply-templates/>
+        </a>
+      </xsl:when>
+      <xsl:otherwise>
+        <span class="bibl">
+          <xsl:apply-templates/>
+        </span>
+      </xsl:otherwise>
+    </xsl:choose>
+  </xsl:template>
+
+  <xsl:template match="tr">
+    <span>
+      <xsl:attribute name="class">
+        <xsl:value-of select="name(.)" />
+      </xsl:attribute>
+      <xsl:attribute name="style">font-weight: bold;</xsl:attribute>
+
+      <xsl:apply-templates/>
+    </span>
+  </xsl:template>
+
+  <xsl:template match="title">
+    <span>
+      <xsl:attribute name="class">
+        <xsl:value-of select="name(.)" />
+      </xsl:attribute>
+      <xsl:attribute name="style">font-style: italic;</xsl:attribute>
+      <xsl:apply-templates/>
+    </span>
+  </xsl:template>
+
+  <xsl:template match="author">
+    <span>
+      <xsl:attribute name="class">
+        <xsl:value-of select="name(.)" />
+      </xsl:attribute>
+      <xsl:attribute name="style">font-variant: small-caps;</xsl:attribute>
+      <xsl:apply-templates/>
+    </span>
+  </xsl:template>
+
+  <!-- TODO: Move nested senses via UL / LI -->
+  <xsl:template match="sense">
+    <div class="sense" style="margin-top: 1.0em;">
+      <span style="margin: 0 0.5em;">
+        <strong><xsl:value-of select="./@n" />).</strong>
+      </span>
+      <xsl:apply-templates/>
+    </div>
+  </xsl:template>
+
+  <!-- TODO: Improve whitespace formatting -->
+  <!-- <xsl:strip-space elements="*" /> -->
+</xsl:stylesheet>
--- backend/scaife_stack_atlas/extractors/extract_lsj.py
+++ backend/scaife_stack_atlas/extractors/extract_lsj.py
@@ -1,11 +1,19 @@
 import json
+import re
+from functools import lru_cache
 from pathlib import Path
 
 from betacode.conv import beta_to_uni
 from lxml import etree
 
+from scaife_viewer.atlas.backports.scaife_viewer.cts.utils import natural_keys
 
-def get_elements(root, nattr=None):
+
+DEBUG = True
+XSL_STYLESHEET_PATH = Path("data/raw/lsj/lsj.xsl")
+
+
+def get_entry_free_elements(root, nattr=None):
     if nattr:
         return root.xpath(f"//entryFree[@id='{nattr}']")
     return root.xpath("//entryFree")
@@ -23,10 +31,15 @@ def stringish(element):
         .strip()
     )
     # TODO: Revisit excessive whitepace stripping; was a problem on Cunliffe and likely an issue here too
+    # TODO: Compare to get_text_from_element
     return " ".join(s.strip() for s in text.split() if s.strip())
 
 
-def healed_citation_urn(urnish):
+def resolvable_urn(urnish):
+    """
+    Returns a URN resolvable by Beyond Translation
+    (e.g., Iliad or Odyssey)
+    """
     # FIXME: This should be done / handled within the app at runtime; shortcut
     # for a demo
     if not urnish:
@@ -56,7 +69,7 @@ def process_citation(c_element, counters):
     bibl_entries = []
     for b_element in c_element.findall("bibl"):
         # TODO: get creative with XSL or other transforms
-        urnish = healed_citation_urn(b_element.attrib.get("n", ""))
+        urnish = resolvable_urn(b_element.attrib.get("n", ""))
         bibl_entries.append((stringish(b_element), urnish))
     assert len(bibl_entries) == 1
 
@@ -108,10 +121,14 @@ def get_entries():
         ),
     ]
     entries = []
-    counters = dict(sense=0, entry=0, citation=0,)
+    counters = dict(
+        sense=0,
+        entry=0,
+        citation=0,
+    )
     for path, nattr in paths_and_nattrs:
         root = etree.parse(path.open("rb"))
-        elements = get_elements(root, nattr)
+        elements = get_entry_free_elements(root, nattr)
         processed = []
         senses = []
         for entry in elements:
@@ -159,8 +176,177 @@ def structural_entries():
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
+def get_text_from_element(element):
+    element_text = " ".join(element.itertext()).strip()
+    # Collapse whitespace
+    return re.sub(r"\s+", " ", element_text)
+
+
+def extract_citations_from_element(element, counters):
+    """
+    Extracts citations by looking for bibl elements.
+
+    If the bibl has a quote, extracts the quoted text
+    as unicode.
+
+    Return type is a list of ATLAS Citations
+    - urn
+    - data
+      - quote
+      - ref
+      - urn
+    """
+    citations = []
+    for bibl in element.xpath(".//bibl[starts-with(@n, 'urn')]"):
+        urn = resolvable_urn(bibl.attrib.get("n"))
+        if not urn:
+            continue
+        citation_text = get_text_from_element(bibl)
+        quote = None
+        quotes = bibl.xpath("preceding-sibling::quote")
+        if quotes:
+            quote = quotes[0]
+            to_unicode(quote)
+            quote = stringish(quote)
+        counters["citation"] += 1
+        citations.append(
+            dict(
+                urn=f"urn:cite2:scaife-viewer:citations.atlas_v1:lsj-{counters['citation']}",
+                data=dict(quote=quote, ref=citation_text, urn=urn),
+            )
+        )
+    return citations
+
+
+def extract_atlas_entry(entry, content, counters):
+    headword = beta_to_uni(entry.attrib.get("key"))
+    counters["entry"] += 1
+    senses = []
+    return dict(
+        headword=headword,
+        data=dict(content=content),
+        senses=senses,
+        citations=extract_citations_from_element(entry, counters),
+        urn=f"urn:cite2:scafife-viewer:dictionary-entries.atlas_v1:lsj-{counters['entry']}",
+    )
+
+
+def get_lsj_paths():
+    # FIXME: vendor data
+    lsj_xml_dir = Path(
+        "/Users/jwegner/Data/development/repos/PerseusDL/lexica/CTS_XML_TEI/perseus/pdllex/grc/lsj/"
+    )
+    return sorted(lsj_xml_dir.glob("*.xml"), key=lambda x: natural_keys(x.name))
+
+
+def get_lsj_entry_free_elements():
+    if DEBUG:
+        paths_and_nattrs = [
+            (
+                # ἀείδω
+                Path("data/raw/lsj/grc.lsj.perseus-eng1.xml"),
+                "n1587",
+            ),
+            (
+                # μῆνις
+                Path("data/raw/lsj/grc.lsj.perseus-eng13.xml"),
+                "n67485",
+            ),
+        ]
+    else:
+        paths_and_nattrs = [(path, None) for path in get_lsj_paths()]
+
+    elements = []
+    for path, nattr in paths_and_nattrs:
+        root = etree.parse(path.open("rb"))
+        elements.extend(get_entry_free_elements(root, nattr))
+    return elements
+
+
+class XSLTransformer:
+    # TODO: Discuss this with @jtauber
+    REPLACEMENTS = [
+        ("α^", "ᾰ"),
+    ]
+
+    def __init__(self, xml):
+        self.xml = xml
+
+    def beta_to_uni(self, ctx, text_selector):
+        value = text_selector[0]
+        value = beta_to_uni(value)
+        for needle, haystack in self.REPLACEMENTS:
+            value = value.replace(needle, haystack)
+        return value
+
+    def catalog_link(self, ctx, value):
+        # TODO: Point this to something within Beyond Translation
+        node = value[0]
+        urn = node.attrib["n"]
+        version_urn = urn.rsplit(":", maxsplit=1)[0]
+        work_urn = version_urn.rsplit(".", maxsplit=1)[0]
+        return f"https://catalog.perseus.org/catalog/{work_urn}/"
+
+    # TODO: Backported from scaife_viewer_core TEIRenderer
+    @lru_cache
+    def render(self):
+        with XSL_STYLESHEET_PATH.open("rb") as f:
+            func_ns = "urn:python-funcs"
+            transform = etree.XSLT(
+                etree.XML(f.read()),
+                extensions={
+                    (func_ns, "beta_to_uni"): self.beta_to_uni,
+                    (func_ns, "catalog_link"): self.catalog_link,
+                },
+            )
+            try:
+                return str(transform(self.xml))
+            except Exception:
+                for error in transform.error_log:
+                    print(error.message, error.line)
+                raise
+
+
+def extract_content(entry, debug=False):
+    # TODO: Revisit transformer instantiation
+    transformer = XSLTransformer(entry)
+    # TODO: Revisit whitespace
+    content = transformer.render().strip()
+    content = " ".join(content.split())
+    if debug:
+        output = Path("data/raw/lsj/output.html")
+        with output.open("w") as f:
+            f.write(content)
+    return content
+
+
+def blob_entries():
+    counters = dict(
+        sense=0,
+        entry=0,
+        citation=0,
+    )
+    extracted_entries = []
+    for pos, entry in enumerate(get_lsj_entry_free_elements()):
+
+        debug = DEBUG and pos == 0
+        content = extract_content(entry, debug=debug)
+        atlas_entry = extract_atlas_entry(entry, content, counters)
+        extracted_entries.append(atlas_entry)
+
+    data = {
+        "label": "A Greek-English Lexicon (LSJ)",
+        "urn": "urn:cite2:scaife-viewer:dictionaries.v1:lsj",
+        "kind": "Dictionary",
+        "entries": extracted_entries,
+    }
+    output_path = Path("data/annotations/dictionaries/lsj-sample.json")
+    with output_path.open("w") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
 def main():
-    structural_entries()
+    blob_entries()
 
 
 if __name__ == "__main__":
