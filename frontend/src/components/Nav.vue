cbdf302109622642148c69d61f31d4302ce5600d
Jake Wegner <jake.wegner@signumu.org>
2023-03-10 16:03:53 -0600

    Add support for an about url

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="$options.aboutUrl" class="top-nav u-flex">
+    <a :href="$options.aboutUrl">{{ $options.aboutText }}</a>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Nav',
+    aboutUrl: process.env.VUE_APP_ABOUT_URL,
+    aboutText: process.env.VUE_APP_ABOUT_TEXT || 'About',
+  };
+</script>
+
+<style lang="scss">
+  .top-nav {
+    border-bottom: 1px solid $gray-300;
+    color: $white;
+    flex-direction: row;
+    justify-content: end;
+    padding: 1em 0;
+    width: 100%;
+    z-index: 100;
+    > * {
+      margin-right: 2em;
+    }
+    a {
+      color: $gray-700;
+      text-decoration: none;
+    }
+  }
+</style>
