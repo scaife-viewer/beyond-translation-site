b9fbaa29c6ab52868e162747ae1c19c80be367b0
Jake Wegner <jake.wegner@signumu.org>
2023-04-25 09:56:26 -0500

    Extract TOC for Antigone

---
+++ backend/scaife_stack_atlas/extractors/extract_antigone_toc.py
@@ -0,0 +1,61 @@
+import json
+from lxml import etree
+from pathlib import Path
+from collections import defaultdict
+
+
+def main():
+    path = Path(
+        "/Users/jwegner/Data/development/repos/scaife-viewer/scaife-viewer/data/cts/PerseusDL-canonical-greekLit-0.0.4176122695-7070a1d/data/tlg0011/tlg002/tlg0011.tlg002.perseus-grc2.xml"
+    )
+
+    TEI_NS = {"TEI": "http://www.tei-c.org/ns/1.0"}
+    parsed = etree.parse(path)
+
+    version_urn = parsed.xpath("//TEI:div[position()=1]", namespaces=TEI_NS)[
+        0
+    ].attrib.get("n")
+    milestones = parsed.xpath("//TEI:milestone", namespaces=TEI_NS)
+    lookup = defaultdict(list)
+    for pos, milestone in enumerate(milestones):
+        #  milestone.xpath("./following::TEI:l[position()=1]", namespaces=TEI_NS)[0].attrib
+        # milestone.xpath("./ancestor::TEI:div[position()=1]", namespaces=TEI_NS)[0].attrib
+        # an alternative here would be to resolve the refsDecls, and then the milestones
+        next_line = milestone.xpath(
+            "./following::TEI:l[position()=1]", namespaces=TEI_NS
+        )[0].attrib.get("n")
+        lookup[pos].append(next_line)
+        try:
+            previous_line = milestone.xpath(
+                "./preceding::TEI:l[position()=1]", namespaces=TEI_NS
+            )[0].attrib.get("n")
+        except IndexError as exception:
+            if pos == 0:
+                pass
+            else:
+                raise exception
+        else:
+            lookup[pos - 1].append(previous_line)
+
+    tocs = []
+    for entry in lookup.values():
+        title = f'lines {"-".join(entry)}'
+        if len(entry) == 1:
+            # this is the last line
+            title = f"{title}ff."
+        toc = dict(title=title, uri=f"{version_urn}:{entry[0]}")
+        tocs.append(toc)
+
+    antigone_toc_path = Path("data/tocs/toc.antigone.json")
+    data = {
+        "@id": "urn:cite:scaife-viewer:toc.antigone",
+        "title": "Antigone (Cards)",
+        "description": "Mapping of cards to lines",
+        "items": tocs,
+    }
+    with antigone_toc_path.open("w") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
