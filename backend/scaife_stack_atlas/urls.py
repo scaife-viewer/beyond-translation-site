b509fc1226a9dbda045e67f16ba8f4d7629bc5ce
Jake Wegner <jake.wegner@signumu.org>
2023-07-03 06:57:09 -0500

    Switch TOCs over to GraphQL

--- backend/scaife_stack_atlas/urls.py
+++ backend/scaife_stack_atlas/urls.py
@@ -3,13 +3,9 @@ from django.urls import include, path, re_path
 from django.contrib import admin
 
 from . import views
-from .tocs.views import serve_toc, tocs_index
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("scaife_viewer.atlas.urls")),
-    path("tocs/<filename>", serve_toc, name="serve_toc"),
-    path("tocs/", tocs_index, name="tocs_index"),
     re_path(r"^", views.FrontendAppView.as_view()),
 ]
