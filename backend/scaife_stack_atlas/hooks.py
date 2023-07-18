c9a3d8bd1cbb1078fc8fa0a967794aa09badf0d3
Jake Wegner <jake.wegner@signumu.org>
2023-07-18 11:06:22 -0500

    Update to make use of get_tokens_from_csv from scaife-viewer-atlas

--- backend/scaife_stack_atlas/hooks.py
+++ backend/scaife_stack_atlas/hooks.py
@@ -1,12 +1,14 @@
 from pathlib import Path
+
 from scaife_viewer.atlas.hooks import DefaultHookSet
 
 
 class ATLASHookSet(DefaultHookSet):
-    def prepare_version_tokens(self, version_urn):
+    def get_prepared_tokens(self, version_urn):
         if version_urn == "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:":
-            from .site_tokenizers import get_tokens_from_csv
+            from scaife_viewer.atlas.tokenizers import get_tokens_from_csv
+
             # FIXME: Remove hard-coded path
             csv_path = Path("killroy.csv")
             return get_tokens_from_csv(version_urn, csv_path)
-        return super().prepare_version_tokens(version_urn)
+        return super().get_prepared_tokens(version_urn)
