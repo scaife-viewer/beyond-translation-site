9c95887e7d85613a3ac7dbba1b33f4c89b024d56
Jake Wegner <jake.wegner@signumu.org>
2023-07-19 16:54:47 -0500

    Customize hookset for tokenization override

--- backend/scaife_stack_atlas/hooks.py
+++ backend/scaife_stack_atlas/hooks.py
@@ -5,9 +5,9 @@ from scaife_viewer.atlas.hooks import DefaultHookSet
 
 class ATLASHookSet(DefaultHookSet):
     def get_prepared_tokens(self, version_urn):
-        # NOTE: Sample override for Iliad tokens shown below:
-        # if version_urn == "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:":
-        #     from scaife_viewer.atlas.tokenizers import get_tokens_from_csv
-        #     csv_path = Path("killroy.csv")
-        #     return get_tokens_from_csv(version_urn, csv_path)
+        # NOTE: Sample override for The life of Omar ben Saeed below
+        if version_urn == "urn:cts:arabicLit:amedsaid1831.dw042.perseus-ara1:":
+            from scaife_viewer.atlas.tokenizers import get_tokens_from_csv
+            csv_path = Path("data/token-overrides/amedsaid1831.dw042.perseus-ara1.csv")
+            return get_tokens_from_csv(version_urn, csv_path)
         return super().get_prepared_tokens(version_urn)
