c86cb513560f6db8441aa2065bd5832b1882830e
Jake Wegner <jake.wegner@signumu.org>
2023-07-19 12:38:18 -0500

    Clarify sample override

--- backend/scaife_stack_atlas/hooks.py
+++ backend/scaife_stack_atlas/hooks.py
@@ -1,14 +1,13 @@
-from pathlib import Path
+from pathlib import Path  # noqa: F401
 
 from scaife_viewer.atlas.hooks import DefaultHookSet
 
 
 class ATLASHookSet(DefaultHookSet):
     def get_prepared_tokens(self, version_urn):
-        if version_urn == "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:":
-            from scaife_viewer.atlas.tokenizers import get_tokens_from_csv
-
-            # FIXME: Remove hard-coded path
-            csv_path = Path("killroy.csv")
-            return get_tokens_from_csv(version_urn, csv_path)
+        # NOTE: Sample override for Iliad tokens shown below:
+        # if version_urn == "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:":
+        #     from scaife_viewer.atlas.tokenizers import get_tokens_from_csv
+        #     csv_path = Path("killroy.csv")
+        #     return get_tokens_from_csv(version_urn, csv_path)
         return super().get_prepared_tokens(version_urn)
