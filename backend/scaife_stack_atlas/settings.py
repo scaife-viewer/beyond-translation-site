d760dd87bb97ea53b8c443f750c22ed7fe5c02bf
Jake Wegner <jake.wegner@signumu.org>
2023-11-14 05:48:36 -0600

    Add Valeria's syntax trees for Bellum Alexandrinum

--- backend/scaife_stack_atlas/settings.py
+++ backend/scaife_stack_atlas/settings.py
@@ -191,7 +191,7 @@ if "SV_ATLAS_INGESTION_CONCURRENCY" in os.environ:
 
 SV_ATLAS_INGESTION_PIPELINE = [
     "scaife_viewer.atlas.importers.versions.import_versions",
-    # "scaife_viewer.atlas.importers.text_annotations.import_text_annotations",
+    "scaife_viewer.atlas.importers.text_annotations.import_text_annotations",
     # "scaife_viewer.atlas.importers.attributions.import_attributions",
     # "scaife_viewer.atlas.importers.metrical_annotations.import_metrical_annotations",
     # "scaife_viewer.atlas.importers.image_annotations.import_image_annotations",
@@ -216,6 +216,7 @@ SV_ATLAS_INGESTION_PIPELINE = [
     # "scaife_stack_atlas.temp.add_anabasis_glosses_to_trees",
     # "scaife_stack_atlas.temp.ingest_balex_extras",
     # "scaife_stack_atlas.temp.update_balex_metadata",
+    "scaife_stack_atlas.temp.load_boano_metadata",
     # "scaife_stack_atlas.temp.add_cgl_css",
 ]
 DB_DATA_PATH = os.environ.get("DB_DATA_PATH", PROJECT_ROOT)
