b256397bbf1eb600fbcff55f2a9b8f1bf16ebdb4
Jake Wegner <jake.wegner@signumu.org>
2023-12-28 14:43:48 -0600

    Add metadata for urn:cite2:beyond-translation:text_annotation_collection.atlas_v1:amedsaid1831-hilleary

--- backend/scaife_stack_atlas/settings.py
+++ backend/scaife_stack_atlas/settings.py
@@ -216,6 +216,7 @@ SV_ATLAS_INGESTION_PIPELINE = [
     "scaife_stack_atlas.temp.ingest_balex_extras",
     "scaife_stack_atlas.temp.update_balex_metadata",
     "scaife_stack_atlas.temp.load_boano_metadata",
+    "scaife_stack_atlas.temp.load_hilleary_metadata",
     "scaife_stack_atlas.temp.add_cgl_css",
 ]
 DB_DATA_PATH = os.environ.get("DB_DATA_PATH", PROJECT_ROOT)
